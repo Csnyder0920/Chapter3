2df4b9c67dbad2ad0641346bc726875363521cbd
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-11-08 10:17:27 -0600

    Added SimpleCalculatorProgram

---
+++ SimpleCalculatorProgram/SimpleCalculatorProgram/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNumber1 = New System.Windows.Forms.Label()
+        Me.lblNumber2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblOperation = New System.Windows.Forms.Label()
+        Me.lblResult1 = New System.Windows.Forms.Label()
+        Me.txtNumber1 = New System.Windows.Forms.TextBox()
+        Me.txtNumber2 = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btnIntegerDivide = New System.Windows.Forms.Button()
+        Me.btnExponent = New System.Windows.Forms.Button()
+        Me.btnModulus = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblNumber1
+        '
+        Me.lblNumber1.AutoSize = True
+        Me.lblNumber1.Location = New System.Drawing.Point(73, 43)
+        Me.lblNumber1.Name = "lblNumber1"
+        Me.lblNumber1.Size = New System.Drawing.Size(56, 13)
+        Me.lblNumber1.TabIndex = 0
+        Me.lblNumber1.Text = "Number 1:"
+        '
+        'lblNumber2
+        '
+        Me.lblNumber2.AutoSize = True
+        Me.lblNumber2.Location = New System.Drawing.Point(73, 169)
+        Me.lblNumber2.Name = "lblNumber2"
+        Me.lblNumber2.Size = New System.Drawing.Size(56, 13)
+        Me.lblNumber2.TabIndex = 1
+        Me.lblNumber2.Text = "Number 2:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(73, 169)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 2
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(73, 233)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(43, 13)
+        Me.lblResult.TabIndex = 3
+        Me.lblResult.Text = "Result: "
+        '
+        'lblOperation
+        '
+        Me.lblOperation.AutoSize = True
+        Me.lblOperation.Location = New System.Drawing.Point(73, 103)
+        Me.lblOperation.Name = "lblOperation"
+        Me.lblOperation.Size = New System.Drawing.Size(53, 13)
+        Me.lblOperation.TabIndex = 4
+        Me.lblOperation.Text = "Operation"
+        '
+        'lblResult1
+        '
+        Me.lblResult1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult1.Location = New System.Drawing.Point(180, 232)
+        Me.lblResult1.Name = "lblResult1"
+        Me.lblResult1.Size = New System.Drawing.Size(141, 23)
+        Me.lblResult1.TabIndex = 5
+        '
+        'txtNumber1
+        '
+        Me.txtNumber1.Location = New System.Drawing.Point(180, 40)
+        Me.txtNumber1.Name = "txtNumber1"
+        Me.txtNumber1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber1.TabIndex = 6
+        '
+        'txtNumber2
+        '
+        Me.txtNumber2.Location = New System.Drawing.Point(180, 169)
+        Me.txtNumber2.Name = "txtNumber2"
+        Me.txtNumber2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber2.TabIndex = 7
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(180, 82)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(34, 34)
+        Me.btnAdd.TabIndex = 8
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(220, 82)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(34, 34)
+        Me.btnSubtract.TabIndex = 9
+        Me.btnSubtract.Text = "-"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Location = New System.Drawing.Point(260, 82)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(34, 34)
+        Me.btnMultiply.TabIndex = 10
+        Me.btnMultiply.Text = "*"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'btnIntegerDivide
+        '
+        Me.btnIntegerDivide.Location = New System.Drawing.Point(180, 122)
+        Me.btnIntegerDivide.Name = "btnIntegerDivide"
+        Me.btnIntegerDivide.Size = New System.Drawing.Size(34, 34)
+        Me.btnIntegerDivide.TabIndex = 11
+        Me.btnIntegerDivide.Text = "/"
+        Me.btnIntegerDivide.UseVisualStyleBackColor = True
+        '
+        'btnExponent
+        '
+        Me.btnExponent.Location = New System.Drawing.Point(220, 122)
+        Me.btnExponent.Name = "btnExponent"
+        Me.btnExponent.Size = New System.Drawing.Size(34, 34)
+        Me.btnExponent.TabIndex = 12
+        Me.btnExponent.Text = "^"
+        Me.btnExponent.UseVisualStyleBackColor = True
+        '
+        'btnModulus
+        '
+        Me.btnModulus.Location = New System.Drawing.Point(260, 122)
+        Me.btnModulus.Name = "btnModulus"
+        Me.btnModulus.Size = New System.Drawing.Size(61, 34)
+        Me.btnModulus.TabIndex = 13
+        Me.btnModulus.Text = "MOD"
+        Me.btnModulus.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(96, 273)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(118, 36)
+        Me.btnClear.TabIndex = 14
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(247, 273)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(118, 36)
+        Me.btnExit.TabIndex = 15
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(409, 321)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnModulus)
+        Me.Controls.Add(Me.btnExponent)
+        Me.Controls.Add(Me.btnIntegerDivide)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtNumber2)
+        Me.Controls.Add(Me.txtNumber1)
+        Me.Controls.Add(Me.lblResult1)
+        Me.Controls.Add(Me.lblOperation)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblNumber2)
+        Me.Controls.Add(Me.lblNumber1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblNumber1 As System.Windows.Forms.Label
+    Friend WithEvents lblNumber2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents lblOperation As System.Windows.Forms.Label
+    Friend WithEvents lblResult1 As System.Windows.Forms.Label
+    Friend WithEvents txtNumber1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNumber2 As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnSubtract As System.Windows.Forms.Button
+    Friend WithEvents btnMultiply As System.Windows.Forms.Button
+    Friend WithEvents btnIntegerDivide As System.Windows.Forms.Button
+    Friend WithEvents btnExponent As System.Windows.Forms.Button
+    Friend WithEvents btnModulus As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ SimpleCalculatorProgram/SimpleCalculatorProgram/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        ' This event handler performs addition.
+        ' Declare a variable to hold the result.
+        Dim dblResult As Double
+
+        ' Indicate "+" as the operation.
+        lblOperation.Text = " + "
+
+        ' Add the two numbers and store the result in dblResult.
+        dblResult = CDbl(txtNumber1.Text) + CDbl(txtNumber2.Text)
+
+        'Display the result.
+        lblResult1.Text = CStr(dblResult)
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        ' This event handler performs subtraction.
+        ' Declare a variable to hold the result.
+        Dim dblResult As Double
+
+        ' Indicate "-" as the operation.
+        lblOperation.Text = " - "
+
+        ' Subtract the two numbers and store the result in dblResult.
+        dblResult = CDbl(txtNumber1.Text) - CDbl(txtNumber2.Text)
+
+        'Display the result.
+        lblResult1.Text = CStr(dblResult)
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        ' This event handler performs multiplication.
+        ' Declare a variable to hold the result.
+        Dim dblResult As Double
+
+        ' Indicate "*" as the operation.
+        lblOperation.Text = " * "
+
+        ' Add the two numbers and store the result in dblResult.
+        dblResult = CDbl(txtNumber1.Text) * CDbl(txtNumber2.Text)
+
+        'Display the result.
+        lblResult1.Text = CStr(dblResult)
+    End Sub
+
+    Private Sub btnIntegerDivide_Click(sender As Object, e As EventArgs) Handles btnIntegerDivide.Click
+        ' This event handler performs integer division.
+        ' Declare a variable to hold the result.
+        Dim dblResult As Double
+
+        ' Indicate "/" as the operation.
+        lblOperation.Text = " / "
+
+        ' Add the two numbers and store the result in dblResult.
+        dblResult = CDbl(txtNumber1.Text) / CDbl(txtNumber2.Text)
+
+        'Display the result.
+        lblResult1.Text = CStr(dblResult)
+    End Sub
+
+    Private Sub btnExponent_Click(sender As Object, e As EventArgs) Handles btnExponent.Click
+        ' This event handler performs Exponential.
+        ' Declare a variable to hold the result.
+        Dim dblResult As Double
+
+        ' Indicate "^" as the operation.
+        lblOperation.Text = " ^ "
+
+        ' Add the two numbers and store the result in dblResult.
+        dblResult = CDbl(txtNumber1.Text) ^ CDbl(txtNumber2.Text)
+
+        'Display the result.
+        lblResult1.Text = CStr(dblResult)
+    End Sub
+
+    Private Sub btnModulus_Click(sender As Object, e As EventArgs) Handles btnModulus.Click
+        ' This event handler performs modulus.
+        ' Declare a variable to hold the result.
+        Dim dblResult As Double
+
+        ' Indicate "Mod" as the operation.
+        lblOperation.Text = " Mod "
+
+        ' Add the two numbers and store the result in dblResult.
+        dblResult = CDbl(txtNumber1.Text) Mod CDbl(txtNumber2.Text)
+
+        'Display the result.
+        lblResult1.Text = CStr(dblResult)
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNumber1.Clear()
+        txtNumber2.Clear()
+        lblResult1.Text = ""
+        lblOperation.Text = "Operation:"
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
