ee6b5810955fc3d885f2897ed4ad44d8e1baa483
Christian Snyder <Csnyder0920@gmail.com>
2017-11-13 20:38:43 -0600

    Added (Partial) StadiumSeatingProgram

--- Programs #1, #2, #3, #8, and #11/StadiumSeatingProgram/StadiumSeatingProgram/Form1.Designer.vb
+++ Programs #1, #2, #3, #8, and #11/StadiumSeatingProgram/StadiumSeatingProgram/Form1.Designer.vb
@@ -23,191 +23,244 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtClassC = New System.Windows.Forms.TextBox()
+        Me.txtClassB = New System.Windows.Forms.TextBox()
+        Me.txtClassA = New System.Windows.Forms.TextBox()
+        Me.lblClassC1 = New System.Windows.Forms.Label()
+        Me.lblClassB1 = New System.Windows.Forms.Label()
+        Me.lblClassA1 = New System.Windows.Forms.Label()
+        Me.lblHeading = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.Label10 = New System.Windows.Forms.Label()
-        Me.Label11 = New System.Windows.Forms.Label()
-        Me.Label12 = New System.Windows.Forms.Label()
+        Me.lblTotalRevenueResult = New System.Windows.Forms.Label()
+        Me.lblClassCResult = New System.Windows.Forms.Label()
+        Me.lblClassBResult = New System.Windows.Forms.Label()
+        Me.lblClassAResult = New System.Windows.Forms.Label()
+        Me.lblClassA2 = New System.Windows.Forms.Label()
+        Me.lblClassB2 = New System.Windows.Forms.Label()
+        Me.lblClassC2 = New System.Windows.Forms.Label()
+        Me.lblTotalRevenue = New System.Windows.Forms.Label()
+        Me.btnCalculateRevenue = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.TextBox3)
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
-        Me.GroupBox1.Controls.Add(Me.Label4)
-        Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Controls.Add(Me.txtClassC)
+        Me.GroupBox1.Controls.Add(Me.txtClassB)
+        Me.GroupBox1.Controls.Add(Me.txtClassA)
+        Me.GroupBox1.Controls.Add(Me.lblClassC1)
+        Me.GroupBox1.Controls.Add(Me.lblClassB1)
+        Me.GroupBox1.Controls.Add(Me.lblClassA1)
+        Me.GroupBox1.Controls.Add(Me.lblHeading)
+        Me.GroupBox1.Location = New System.Drawing.Point(18, 18)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(232, 214)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox1.Size = New System.Drawing.Size(348, 329)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Tickets Sold"
         '
+        'txtClassC
+        '
+        Me.txtClassC.Location = New System.Drawing.Point(144, 257)
+        Me.txtClassC.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtClassC.Name = "txtClassC"
+        Me.txtClassC.Size = New System.Drawing.Size(144, 26)
+        Me.txtClassC.TabIndex = 6
+        '
+        'txtClassB
+        '
+        Me.txtClassB.Location = New System.Drawing.Point(144, 202)
+        Me.txtClassB.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtClassB.Name = "txtClassB"
+        Me.txtClassB.Size = New System.Drawing.Size(144, 26)
+        Me.txtClassB.TabIndex = 5
+        '
+        'txtClassA
+        '
+        Me.txtClassA.Location = New System.Drawing.Point(144, 143)
+        Me.txtClassA.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtClassA.Name = "txtClassA"
+        Me.txtClassA.Size = New System.Drawing.Size(144, 26)
+        Me.txtClassA.TabIndex = 4
+        '
+        'lblClassC1
+        '
+        Me.lblClassC1.AutoSize = True
+        Me.lblClassC1.Location = New System.Drawing.Point(57, 260)
+        Me.lblClassC1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassC1.Name = "lblClassC1"
+        Me.lblClassC1.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassC1.TabIndex = 3
+        Me.lblClassC1.Text = "Class C"
+        '
+        'lblClassB1
+        '
+        Me.lblClassB1.AutoSize = True
+        Me.lblClassB1.Location = New System.Drawing.Point(57, 204)
+        Me.lblClassB1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassB1.Name = "lblClassB1"
+        Me.lblClassB1.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassB1.TabIndex = 2
+        Me.lblClassB1.Text = "Class B"
+        '
+        'lblClassA1
+        '
+        Me.lblClassA1.AutoSize = True
+        Me.lblClassA1.Location = New System.Drawing.Point(57, 146)
+        Me.lblClassA1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassA1.Name = "lblClassA1"
+        Me.lblClassA1.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassA1.TabIndex = 1
+        Me.lblClassA1.Text = "Class A"
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Location = New System.Drawing.Point(54, 66)
+        Me.lblHeading.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(234, 40)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Enter the number of tickets sold" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "for each class of seats"
+        '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.Label12)
-        Me.GroupBox2.Controls.Add(Me.Label11)
-        Me.GroupBox2.Controls.Add(Me.Label10)
-        Me.GroupBox2.Controls.Add(Me.Label9)
-        Me.GroupBox2.Controls.Add(Me.Label5)
-        Me.GroupBox2.Controls.Add(Me.Label6)
-        Me.GroupBox2.Controls.Add(Me.Label7)
-        Me.GroupBox2.Controls.Add(Me.Label8)
-        Me.GroupBox2.Location = New System.Drawing.Point(268, 12)
+        Me.GroupBox2.Controls.Add(Me.lblTotalRevenueResult)
+        Me.GroupBox2.Controls.Add(Me.lblClassCResult)
+        Me.GroupBox2.Controls.Add(Me.lblClassBResult)
+        Me.GroupBox2.Controls.Add(Me.lblClassAResult)
+        Me.GroupBox2.Controls.Add(Me.lblClassA2)
+        Me.GroupBox2.Controls.Add(Me.lblClassB2)
+        Me.GroupBox2.Controls.Add(Me.lblClassC2)
+        Me.GroupBox2.Controls.Add(Me.lblTotalRevenue)
+        Me.GroupBox2.Location = New System.Drawing.Point(402, 18)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(232, 214)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox2.Size = New System.Drawing.Size(348, 329)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Revenue Generated"
         '
-        'Label1
+        'lblTotalRevenueResult
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(21, 36)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.lblTotalRevenueResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalRevenueResult.Location = New System.Drawing.Point(174, 282)
+        Me.lblTotalRevenueResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblTotalRevenueResult.Name = "lblTotalRevenueResult"
+        Me.lblTotalRevenueResult.Size = New System.Drawing.Size(150, 35)
+        Me.lblTotalRevenueResult.TabIndex = 10
         '
-        'Label2
+        'lblClassCResult
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(21, 100)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
+        Me.lblClassCResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassCResult.Location = New System.Drawing.Point(174, 191)
+        Me.lblClassCResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassCResult.Name = "lblClassCResult"
+        Me.lblClassCResult.Size = New System.Drawing.Size(150, 35)
+        Me.lblClassCResult.TabIndex = 9
         '
-        'Label3
+        'lblClassBResult
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(21, 138)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Label3"
+        Me.lblClassBResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassBResult.Location = New System.Drawing.Point(174, 135)
+        Me.lblClassBResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassBResult.Name = "lblClassBResult"
+        Me.lblClassBResult.Size = New System.Drawing.Size(150, 35)
+        Me.lblClassBResult.TabIndex = 8
         '
-        'Label4
+        'lblClassAResult
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(21, 174)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Label4"
+        Me.lblClassAResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassAResult.Location = New System.Drawing.Point(174, 86)
+        Me.lblClassAResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassAResult.Name = "lblClassAResult"
+        Me.lblClassAResult.Size = New System.Drawing.Size(150, 35)
+        Me.lblClassAResult.TabIndex = 2
         '
-        'Label5
+        'lblClassA2
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(32, 57)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(39, 13)
-        Me.Label5.TabIndex = 4
-        Me.Label5.Text = "Label5"
+        Me.lblClassA2.AutoSize = True
+        Me.lblClassA2.Location = New System.Drawing.Point(84, 86)
+        Me.lblClassA2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassA2.Name = "lblClassA2"
+        Me.lblClassA2.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassA2.TabIndex = 4
+        Me.lblClassA2.Text = "Class A"
         '
-        'Label6
+        'lblClassB2
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(32, 89)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(39, 13)
-        Me.Label6.TabIndex = 5
-        Me.Label6.Text = "Label6"
+        Me.lblClassB2.AutoSize = True
+        Me.lblClassB2.Location = New System.Drawing.Point(84, 136)
+        Me.lblClassB2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassB2.Name = "lblClassB2"
+        Me.lblClassB2.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassB2.TabIndex = 5
+        Me.lblClassB2.Text = "Class B"
         '
-        'Label7
+        'lblClassC2
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(32, 125)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(39, 13)
-        Me.Label7.TabIndex = 6
-        Me.Label7.Text = "Label7"
+        Me.lblClassC2.AutoSize = True
+        Me.lblClassC2.Location = New System.Drawing.Point(84, 192)
+        Me.lblClassC2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassC2.Name = "lblClassC2"
+        Me.lblClassC2.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassC2.TabIndex = 6
+        Me.lblClassC2.Text = "Class C"
         '
-        'Label8
+        'lblTotalRevenue
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(32, 184)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(39, 13)
-        Me.Label8.TabIndex = 7
-        Me.Label8.Text = "Label8"
+        Me.lblTotalRevenue.AutoSize = True
+        Me.lblTotalRevenue.Location = New System.Drawing.Point(31, 283)
+        Me.lblTotalRevenue.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblTotalRevenue.Name = "lblTotalRevenue"
+        Me.lblTotalRevenue.Size = New System.Drawing.Size(116, 20)
+        Me.lblTotalRevenue.TabIndex = 7
+        Me.lblTotalRevenue.Text = "Total Revenue:"
         '
-        'TextBox1
+        'btnCalculateRevenue
         '
-        Me.TextBox1.Location = New System.Drawing.Point(96, 93)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(97, 20)
-        Me.TextBox1.TabIndex = 4
+        Me.btnCalculateRevenue.Location = New System.Drawing.Point(91, 377)
+        Me.btnCalculateRevenue.Name = "btnCalculateRevenue"
+        Me.btnCalculateRevenue.Size = New System.Drawing.Size(227, 55)
+        Me.btnCalculateRevenue.TabIndex = 2
+        Me.btnCalculateRevenue.Text = "Calculate Revenue"
+        Me.btnCalculateRevenue.UseVisualStyleBackColor = True
         '
-        'TextBox2
+        'btnClear
         '
-        Me.TextBox2.Location = New System.Drawing.Point(96, 131)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(97, 20)
-        Me.TextBox2.TabIndex = 5
+        Me.btnClear.Location = New System.Drawing.Point(338, 377)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(125, 55)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
-        'TextBox3
+        'btnExit
         '
-        Me.TextBox3.Location = New System.Drawing.Point(96, 167)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(97, 20)
-        Me.TextBox3.TabIndex = 6
-        '
-        'Label9
-        '
-        Me.Label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label9.Location = New System.Drawing.Point(116, 56)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(100, 23)
-        Me.Label9.TabIndex = 2
-        '
-        'Label10
-        '
-        Me.Label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label10.Location = New System.Drawing.Point(116, 88)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(100, 23)
-        Me.Label10.TabIndex = 8
-        '
-        'Label11
-        '
-        Me.Label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label11.Location = New System.Drawing.Point(116, 124)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(100, 23)
-        Me.Label11.TabIndex = 9
-        '
-        'Label12
-        '
-        Me.Label12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label12.Location = New System.Drawing.Point(116, 183)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(100, 23)
-        Me.Label12.TabIndex = 10
+        Me.btnExit.Location = New System.Drawing.Point(490, 377)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(125, 55)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(512, 330)
+        Me.ClientSize = New System.Drawing.Size(768, 508)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculateRevenue)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
@@ -219,20 +272,22 @@ Partial Class Form1
     End Sub
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label12 As System.Windows.Forms.Label
-    Friend WithEvents Label11 As System.Windows.Forms.Label
-    Friend WithEvents Label10 As System.Windows.Forms.Label
-    Friend WithEvents Label9 As System.Windows.Forms.Label
-    Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents Label6 As System.Windows.Forms.Label
-    Friend WithEvents Label7 As System.Windows.Forms.Label
-    Friend WithEvents Label8 As System.Windows.Forms.Label
-
+    Friend WithEvents txtClassC As System.Windows.Forms.TextBox
+    Friend WithEvents txtClassB As System.Windows.Forms.TextBox
+    Friend WithEvents txtClassA As System.Windows.Forms.TextBox
+    Friend WithEvents lblClassC1 As System.Windows.Forms.Label
+    Friend WithEvents lblClassB1 As System.Windows.Forms.Label
+    Friend WithEvents lblClassA1 As System.Windows.Forms.Label
+    Friend WithEvents lblHeading As System.Windows.Forms.Label
+    Friend WithEvents lblTotalRevenueResult As System.Windows.Forms.Label
+    Friend WithEvents lblClassCResult As System.Windows.Forms.Label
+    Friend WithEvents lblClassBResult As System.Windows.Forms.Label
+    Friend WithEvents lblClassAResult As System.Windows.Forms.Label
+    Friend WithEvents lblClassA2 As System.Windows.Forms.Label
+    Friend WithEvents lblClassB2 As System.Windows.Forms.Label
+    Friend WithEvents lblClassC2 As System.Windows.Forms.Label
+    Friend WithEvents lblTotalRevenue As System.Windows.Forms.Label
+    Friend WithEvents btnCalculateRevenue As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
 End Class
--- Programs #1, #2, #3, #8, and #11/StadiumSeatingProgram/StadiumSeatingProgram/Form1.vb
+++ Programs #1, #2, #3, #8, and #11/StadiumSeatingProgram/StadiumSeatingProgram/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
+    Private Sub btnCalculateRevenue_Click(sender As Object, e As EventArgs) Handles btnCalculateRevenue.Click
+        ' Class A seats cost $15 each
+        ' Class B seats cost $12 each
+        ' Class C seats cost $9 each
 
+        txtClassA.Text = lblClassAResult.Text
+
+    End Sub
 End Class
