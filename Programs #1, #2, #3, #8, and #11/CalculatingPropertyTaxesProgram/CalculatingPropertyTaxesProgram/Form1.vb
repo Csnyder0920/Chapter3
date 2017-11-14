5e27967ec14d48fe424260a913c9e257bce9ea23
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-11-14 11:23:51 -0600

    Added  CalculatingPropertyTaxesProgram

---
+++ Programs #1, #2, #3, #8, and #11/CalculatingPropertyTaxesProgram/CalculatingPropertyTaxesProgram/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub btnShowTax_Click(sender As Object, e As EventArgs) Handles btnShowTax.Click
+        Dim PropertyValue, sixtyPercentValue, taxValue As Double
+        Const TAX_RATE As Double = 0.64
+
+        Try
+            PropertyValue = txtActualPropertyValue.Text
+            sixtyPercentValue = PropertyValue * 0.6
+
+            taxValue = (sixtyPercentValue / 100) * TAX_RATE
+
+            lblAssesmentAndTax.Text = taxValue.ToString("c")
+        Catch
+            MessageBox.Show("ERROR: please enter values")
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtActualPropertyValue.Clear()
+        lblAssesmentAndTax.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
