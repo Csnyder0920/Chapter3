e5c6237807b901979e73cb5f9c28d30714d53671
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-11-14 10:16:03 -0600

    Added StadiumSeatingProgram

--- Programs #1, #2, #3, #8, and #11/StadiumSeatingProgram/StadiumSeatingProgram/Form1.Designer.vb
+++ Programs #1, #2, #3, #8, and #11/StadiumSeatingProgram/StadiumSeatingProgram/Form1.Designer.vb
@@ -55,76 +55,67 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.lblClassB1)
         Me.GroupBox1.Controls.Add(Me.lblClassA1)
         Me.GroupBox1.Controls.Add(Me.lblHeading)
-        Me.GroupBox1.Location = New System.Drawing.Point(18, 18)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.GroupBox1.Size = New System.Drawing.Size(348, 329)
+        Me.GroupBox1.Size = New System.Drawing.Size(232, 214)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Tickets Sold"
         '
         'txtClassC
         '
-        Me.txtClassC.Location = New System.Drawing.Point(144, 257)
-        Me.txtClassC.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtClassC.Location = New System.Drawing.Point(96, 167)
         Me.txtClassC.Name = "txtClassC"
-        Me.txtClassC.Size = New System.Drawing.Size(144, 26)
+        Me.txtClassC.Size = New System.Drawing.Size(97, 20)
         Me.txtClassC.TabIndex = 6
         '
         'txtClassB
         '
-        Me.txtClassB.Location = New System.Drawing.Point(144, 202)
-        Me.txtClassB.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtClassB.Location = New System.Drawing.Point(96, 131)
         Me.txtClassB.Name = "txtClassB"
-        Me.txtClassB.Size = New System.Drawing.Size(144, 26)
+        Me.txtClassB.Size = New System.Drawing.Size(97, 20)
         Me.txtClassB.TabIndex = 5
         '
         'txtClassA
         '
-        Me.txtClassA.Location = New System.Drawing.Point(144, 143)
-        Me.txtClassA.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtClassA.Location = New System.Drawing.Point(96, 93)
         Me.txtClassA.Name = "txtClassA"
-        Me.txtClassA.Size = New System.Drawing.Size(144, 26)
+        Me.txtClassA.Size = New System.Drawing.Size(97, 20)
         Me.txtClassA.TabIndex = 4
         '
         'lblClassC1
         '
         Me.lblClassC1.AutoSize = True
-        Me.lblClassC1.Location = New System.Drawing.Point(57, 260)
-        Me.lblClassC1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassC1.Location = New System.Drawing.Point(38, 169)
         Me.lblClassC1.Name = "lblClassC1"
-        Me.lblClassC1.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassC1.Size = New System.Drawing.Size(42, 13)
         Me.lblClassC1.TabIndex = 3
         Me.lblClassC1.Text = "Class C"
         '
         'lblClassB1
         '
         Me.lblClassB1.AutoSize = True
-        Me.lblClassB1.Location = New System.Drawing.Point(57, 204)
-        Me.lblClassB1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassB1.Location = New System.Drawing.Point(38, 133)
         Me.lblClassB1.Name = "lblClassB1"
-        Me.lblClassB1.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassB1.Size = New System.Drawing.Size(42, 13)
         Me.lblClassB1.TabIndex = 2
         Me.lblClassB1.Text = "Class B"
         '
         'lblClassA1
         '
         Me.lblClassA1.AutoSize = True
-        Me.lblClassA1.Location = New System.Drawing.Point(57, 146)
-        Me.lblClassA1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassA1.Location = New System.Drawing.Point(38, 95)
         Me.lblClassA1.Name = "lblClassA1"
-        Me.lblClassA1.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassA1.Size = New System.Drawing.Size(42, 13)
         Me.lblClassA1.TabIndex = 1
         Me.lblClassA1.Text = "Class A"
         '
         'lblHeading
         '
         Me.lblHeading.AutoSize = True
-        Me.lblHeading.Location = New System.Drawing.Point(54, 66)
-        Me.lblHeading.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblHeading.Location = New System.Drawing.Point(36, 43)
         Me.lblHeading.Name = "lblHeading"
-        Me.lblHeading.Size = New System.Drawing.Size(234, 40)
+        Me.lblHeading.Size = New System.Drawing.Size(156, 26)
         Me.lblHeading.TabIndex = 0
         Me.lblHeading.Text = "Enter the number of tickets sold" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "for each class of seats"
         '
@@ -138,11 +129,9 @@ Partial Class Form1
         Me.GroupBox2.Controls.Add(Me.lblClassB2)
         Me.GroupBox2.Controls.Add(Me.lblClassC2)
         Me.GroupBox2.Controls.Add(Me.lblTotalRevenue)
-        Me.GroupBox2.Location = New System.Drawing.Point(402, 18)
-        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox2.Location = New System.Drawing.Point(268, 12)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.GroupBox2.Size = New System.Drawing.Size(348, 329)
+        Me.GroupBox2.Size = New System.Drawing.Size(232, 214)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Revenue Generated"
@@ -150,117 +139,111 @@ Partial Class Form1
         'lblTotalRevenueResult
         '
         Me.lblTotalRevenueResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblTotalRevenueResult.Location = New System.Drawing.Point(174, 282)
-        Me.lblTotalRevenueResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblTotalRevenueResult.Location = New System.Drawing.Point(116, 183)
         Me.lblTotalRevenueResult.Name = "lblTotalRevenueResult"
-        Me.lblTotalRevenueResult.Size = New System.Drawing.Size(150, 35)
+        Me.lblTotalRevenueResult.Size = New System.Drawing.Size(100, 23)
         Me.lblTotalRevenueResult.TabIndex = 10
         '
         'lblClassCResult
         '
         Me.lblClassCResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblClassCResult.Location = New System.Drawing.Point(174, 191)
-        Me.lblClassCResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassCResult.Location = New System.Drawing.Point(116, 124)
         Me.lblClassCResult.Name = "lblClassCResult"
-        Me.lblClassCResult.Size = New System.Drawing.Size(150, 35)
+        Me.lblClassCResult.Size = New System.Drawing.Size(100, 23)
         Me.lblClassCResult.TabIndex = 9
         '
         'lblClassBResult
         '
         Me.lblClassBResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblClassBResult.Location = New System.Drawing.Point(174, 135)
-        Me.lblClassBResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassBResult.Location = New System.Drawing.Point(116, 88)
         Me.lblClassBResult.Name = "lblClassBResult"
-        Me.lblClassBResult.Size = New System.Drawing.Size(150, 35)
+        Me.lblClassBResult.Size = New System.Drawing.Size(100, 23)
         Me.lblClassBResult.TabIndex = 8
         '
         'lblClassAResult
         '
         Me.lblClassAResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblClassAResult.Location = New System.Drawing.Point(174, 86)
-        Me.lblClassAResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassAResult.Location = New System.Drawing.Point(116, 56)
         Me.lblClassAResult.Name = "lblClassAResult"
-        Me.lblClassAResult.Size = New System.Drawing.Size(150, 35)
+        Me.lblClassAResult.Size = New System.Drawing.Size(100, 23)
         Me.lblClassAResult.TabIndex = 2
         '
         'lblClassA2
         '
         Me.lblClassA2.AutoSize = True
-        Me.lblClassA2.Location = New System.Drawing.Point(84, 86)
-        Me.lblClassA2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassA2.Location = New System.Drawing.Point(56, 56)
         Me.lblClassA2.Name = "lblClassA2"
-        Me.lblClassA2.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassA2.Size = New System.Drawing.Size(42, 13)
         Me.lblClassA2.TabIndex = 4
         Me.lblClassA2.Text = "Class A"
         '
         'lblClassB2
         '
         Me.lblClassB2.AutoSize = True
-        Me.lblClassB2.Location = New System.Drawing.Point(84, 136)
-        Me.lblClassB2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassB2.Location = New System.Drawing.Point(56, 88)
         Me.lblClassB2.Name = "lblClassB2"
-        Me.lblClassB2.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassB2.Size = New System.Drawing.Size(42, 13)
         Me.lblClassB2.TabIndex = 5
         Me.lblClassB2.Text = "Class B"
         '
         'lblClassC2
         '
         Me.lblClassC2.AutoSize = True
-        Me.lblClassC2.Location = New System.Drawing.Point(84, 192)
-        Me.lblClassC2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClassC2.Location = New System.Drawing.Point(56, 125)
         Me.lblClassC2.Name = "lblClassC2"
-        Me.lblClassC2.Size = New System.Drawing.Size(63, 20)
+        Me.lblClassC2.Size = New System.Drawing.Size(42, 13)
         Me.lblClassC2.TabIndex = 6
         Me.lblClassC2.Text = "Class C"
         '
         'lblTotalRevenue
         '
         Me.lblTotalRevenue.AutoSize = True
-        Me.lblTotalRevenue.Location = New System.Drawing.Point(31, 283)
-        Me.lblTotalRevenue.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblTotalRevenue.Location = New System.Drawing.Point(21, 184)
         Me.lblTotalRevenue.Name = "lblTotalRevenue"
-        Me.lblTotalRevenue.Size = New System.Drawing.Size(116, 20)
+        Me.lblTotalRevenue.Size = New System.Drawing.Size(81, 13)
         Me.lblTotalRevenue.TabIndex = 7
         Me.lblTotalRevenue.Text = "Total Revenue:"
         '
         'btnCalculateRevenue
         '
-        Me.btnCalculateRevenue.Location = New System.Drawing.Point(91, 377)
+        Me.btnCalculateRevenue.Location = New System.Drawing.Point(61, 245)
+        Me.btnCalculateRevenue.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnCalculateRevenue.Name = "btnCalculateRevenue"
-        Me.btnCalculateRevenue.Size = New System.Drawing.Size(227, 55)
+        Me.btnCalculateRevenue.Size = New System.Drawing.Size(151, 36)
         Me.btnCalculateRevenue.TabIndex = 2
         Me.btnCalculateRevenue.Text = "Calculate Revenue"
         Me.btnCalculateRevenue.UseVisualStyleBackColor = True
         '
         'btnClear
         '
-        Me.btnClear.Location = New System.Drawing.Point(338, 377)
+        Me.btnClear.Location = New System.Drawing.Point(225, 245)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(125, 55)
+        Me.btnClear.Size = New System.Drawing.Size(83, 36)
         Me.btnClear.TabIndex = 3
         Me.btnClear.Text = "Clear"
         Me.btnClear.UseVisualStyleBackColor = True
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(490, 377)
+        Me.btnExit.Location = New System.Drawing.Point(327, 245)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(125, 55)
+        Me.btnExit.Size = New System.Drawing.Size(83, 36)
         Me.btnExit.TabIndex = 4
         Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(768, 508)
+        Me.ClientSize = New System.Drawing.Size(512, 330)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.btnCalculateRevenue)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
--- Programs #1, #2, #3, #8, and #11/StadiumSeatingProgram/StadiumSeatingProgram/Form1.vb
+++ Programs #1, #2, #3, #8, and #11/StadiumSeatingProgram/StadiumSeatingProgram/Form1.vb
@@ -4,7 +4,38 @@
         ' Class B seats cost $12 each
         ' Class C seats cost $9 each
 
-        txtClassA.Text = lblClassAResult.Text
+        Dim dblClassA As Double
+        Dim dblClassB As Double
+        Dim dblClassC As Double
+        Dim dblTotal As Double
 
+        dblClassA = txtClassA.Text
+        lblClassAResult.Text = (dblClassA * 15)
+        dblClassA = lblClassAResult.Text
+
+        dblClassB = txtClassB.Text
+        lblClassBResult.Text = (dblClassB * 12)
+        dblClassB = lblClassBResult.Text
+
+        dblClassC = txtClassC.Text
+        lblClassCResult.Text = (dblClassC * 9)
+        dblClassC = lblClassCResult.Text
+
+        dblTotal = dblClassA + dblClassB + dblClassC
+        lblTotalRevenueResult.Text = dblTotal
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtClassA.Clear()
+        txtClassB.Clear()
+        txtClassC.Clear()
+        lblClassAResult.Text = ""
+        lblClassBResult.Text = ""
+        lblClassCResult.Text = ""
+        lblTotalRevenueResult.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
     End Sub
 End Class
