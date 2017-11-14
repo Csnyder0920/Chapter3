e7bd33222b92c195841960f38cad3c98e0c4c3f7
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-11-14 10:49:22 -0600

    AddedCelsiusToFahrenheitProgram

---
+++ Programs #1, #2, #3, #8, and #11/CelsiusToFarhenheitProgram/CelsiusToFarhenheitProgram/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub btnShowFahrenheit_Click(sender As Object, e As EventArgs) Handles btnShowFahrenheit.Click
+        Dim dblFahrenheit As Double
+        Dim dblCelsius As Double
+
+        dblCelsius = txtCelsius.Text
+        dblFahrenheit = 1.8 * dblCelsius + 32
+        lblShowFahrenheit.Text = dblFahrenheit
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtCelsius.Clear()
+        lblShowFahrenheit.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
